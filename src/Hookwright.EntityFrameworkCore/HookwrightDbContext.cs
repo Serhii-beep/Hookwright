33d9d1dbb91c574a30c20cf357c97bfb9616609b
peskosergij <peskosergij@gmail.com>
2026-08-18 15:24:28 -0400

    feat(efcore): add the EF Core project and context

---
+++ src/Hookwright.EntityFrameworkCore/HookwrightDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hookwright.EntityFrameworkCore;
+
+/// <summary>
+/// Hookwright's tables in a context of their own.
+/// </summary>
+public class HookwrightDbContext : DbContext
+{
+    /// <summary>
+    /// Creates a context
+    /// </summary>
+    public HookwrightDbContext(DbContextOptions<HookwrightDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    /// <summary>
+    /// Creates a context for a derived type.
+    /// </summary>
+    protected HookwrightDbContext(DbContextOptions options)
+        : base(options)
+    {
+
+    }
+
+    /// <inheritdoc />
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(HookwrightDbContext).Assembly);
+    }
+}
